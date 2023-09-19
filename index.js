fe23029607eb7a5229e37b40899c2c07a137ef4b
LeonGitonga <leon.gitonga@student.moringaschool.com>
2023-09-19 08:51:57 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+console.log(multiply);
+
+function generateRandomIntegerGreaterThanZero() {
+    let randomFloat = Math.random();
+
+    let randomInteger = Math.floor(randomFloat * Number.MAX_SAFE_INTEGER);
+    return randomInteger
+}
+
+let random = generateRandomIntegerGreaterThanZero();
+console.log(random);
+
+let num3 = 14;
+let num4 = 10;
+
+let mod = num3 % num4;
+
+let numbers = [6, 12, 7, 3, 20, 11];
+let max = Math.max(...numbers);
+    
+
+    
+    
